005c987dcd725944ce2a9aaeeb3ebe64f05b9206
minphone <dev.minphon@gmail.com>
2018-04-18 15:03:24 +0630

    Add API for user register

---
+++ app/controllers/api/v1/facebook_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::FacebookController < Api::V1::BaseController
+
+  def create
+
+    if User.find_by_email(params[:email]).nil?
+
+      user = User.create(username: params[:username], email: params[:email], 
+        gender: params[:gender], region_id: params[:region_id],
+        facebook_id: params[:fb_id])
+
+      if @user.nil?
+        render json: user.attributes.merge({
+          :error => user.errors.full_messages.first,
+          :is_verified => false
+        })
+      else
+        render json: user.attributes.merge({
+          :error => "",
+          :is_verified => true
+        })
+      end
+
+    else
+      user = User.find_by_email(params[:email])
+      render json: user.attributes.merge({
+        :error => "",
+        :is_verified => true
+      })
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :regions, only: [ :index ]
+      resources :facebook, :path => "facebook_login", only: [ :create ]
     end
   end
 
